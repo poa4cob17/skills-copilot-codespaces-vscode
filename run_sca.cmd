0a7de0b55cda9580975cd752a6db7e7a6408d4f2
POA4COB <anandkumar.ponnusamy@in.bosch.com>
2025-02-07 13:53:49 +0530

    added template

---
+++ run_sca.cmd
@@ -0,0 +1,21 @@
+@REM set source files
+@REM set autosar files
+@REM set additional files
+@REM set ref_projects or mini pst
+@REM set SWC or SW Unit name
+
+
+@REM call adapter generator to generate swc_adapter files for given SWC
+
+
+@REM call RTE 
+call run_compiler.cmd
+
+@REM call compiler
+
+
+@REM call code guide
+
+
+@REM call SWA
+
